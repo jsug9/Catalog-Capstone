b444687b2337a7898eb7480aa16a1093044797d4
Augusto <jesus_augga_99@icloud.com>
2022-06-27 15:32:18 -0500

    fix linter errors

--- Classes/item.rb
+++ Classes/item.rb
@@ -3,13 +3,13 @@ require 'securerandom'
 class Item
   attr_accessor :id, :genre, :author, :source, :label, :publish_date, :archived
 
-  def initialize(genre, author, source, label, publish_date, archived)
+  def initialize(genre, author, source, label, publish_date)
     @id = SecureRandom.uuid
     @genre = genre
     @author = author
     @source = source
     @label = label
     @publish_date = publish_date
-    @archived = archived
+    @archived = false
   end
 end
