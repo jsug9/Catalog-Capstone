3d49ba031ffdbcd0c5f073f520c81ec521d31886
Augusto <jesus_augga_99@icloud.com>
2022-06-27 15:26:19 -0500

    add item properties

--- Classes/item.rb
+++ Classes/item.rb
@@ -1,3 +1,15 @@
+require 'securerandom'
+
 class Item
-  
+  attr_accessor :id, :genre, :author, :source, :label, :publish_date, :archived
+
+  def initialize(genre, author, source, label, publish_date, archived)
+    @id = SecureRandom.uuid
+    @genre = genre
+    @author = author
+    @source = source
+    @label = label
+    @publish_date = publish_date
+    @archived = archived
+  end
 end
