d4dbaad51512d1ddc2c12d2584524c8950b141e0
Augusto <jesus_augga_99@icloud.com>
2022-06-27 23:34:22 -0500

    add properties to genre c,ass

--- Classes/genre.rb
+++ Classes/genre.rb
@@ -1,3 +1,15 @@
-class genre
-  
+require 'securerandom'
+require_relative 'item'
+
+class Genre
+  def initialize(name)
+    @id = SecureRandom.uuid
+    @name = name
+    @items = []
+  end
+
+  def add_item(item)
+    @items << item unless @items.include?(item)
+    item.add_genre(self) unless item.genres.include?(self)
+  end
 end
