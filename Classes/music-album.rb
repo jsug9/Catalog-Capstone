7abf22e80ede8415bfec0202f45ffbad2464fe64
Augusto <jesus_augga_99@icloud.com>
2022-06-27 23:08:32 -0500

    update item class

--- Classes/music-album.rb
+++ Classes/music-album.rb
@@ -1,3 +1,8 @@
-class MusicAlbum
-  
+require_relative 'item'
+
+class MusicAlbum < Item
+  def initialize(genre, author, source, label, publish_date, on_spotify)
+    super(genre, author, source, label, publish_date)
+    @on_spotify = on_spotify
+  end
 end
