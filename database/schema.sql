b21998a44bd384dd78b14e70d9c2b35c666d0bcc
sediqullahbadakhsh <sediqullah.badakhsh@gmail.com>
2022-06-29 03:07:07 +0430

    create book and label classes

--- database/schema.sql
+++ database/schema.sql
@@ -23,3 +23,24 @@ CREATE TABLE music_albums (
     name text,
     on_spotify boolean
 );
+
+--Lable Table
+CREATE TABLE label(
+  id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  title VARCHAR(100),
+  color VARCHAR(100)
+);
+
+-- Book Table
+CREATE TABLE book(
+  id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  publish_date DATE,
+  publisher VARCHAR(100),
+  cover_state VARCHAR(10),
+  genre_id INT,
+  author_id INT,
+  label_id INT,
+  FOREIGN KEY (genre_id) REFERENCES genre(id) ON DELETE SET NULL ON UPDATE CASCADE,
+  FOREIGN KEY (author_id) REFERENCES author(id) ON DELETE SET NULL ON UPDATE CASCADE,
+  FOREIGN KEY (label_id) REFERENCES label(id) ON DELETE SET NULL ON UPDATE CASCADE
+);
