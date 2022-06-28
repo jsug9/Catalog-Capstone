e470a52d1cf9eb0624e9270c828016401525bf2d
Augusto <jesus_augga_99@icloud.com>
2022-06-28 00:11:16 -0500

    add tests for music album

---
+++ spec/music_album_spec.rb
@@ -0,0 +1,26 @@
+require './Classes/music_album'
+
+describe MusicAlbum do
+  let(:music_album) { MusicAlbum.new(12, true) }
+
+  it 'should be an instance of MusicAlbum' do
+    expect(music_album).to be_an_instance_of(MusicAlbum)
+  end
+
+  it 'should be initialized with a publish date' do
+    expect(music_album.publish_date).to eq(12)
+  end
+
+  it 'should be initialized with a on_spotify property' do
+    expect(music_album.on_spotify).to eq(true)
+  end
+
+  it 'should be initialized with an id' do
+    expect(music_album.id).to be_a(String)
+  end
+
+  it 'should be able to be archived' do
+    music_album.move_to_archive
+    expect(music_album.archived).to eq(true)
+  end
+end
