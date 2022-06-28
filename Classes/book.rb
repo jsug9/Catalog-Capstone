82ad2066180885de715bf325060cd5fba2bfcdb0
sediqullahbadakhsh <sediqullah.badakhsh@gmail.com>
2022-06-28 20:15:38 +0430

    set constructor for the book class

--- Classes/book.rb
+++ Classes/book.rb
@@ -1,7 +1,10 @@
-class Book
+require_relative './item'
+
+class Book < Item
   attr_accessor :publisher, :cover_state
 
-  def initialize(publisher, cover_state)
+  def initialize(publisher, cover_state, id, publish_date)
+    super(id, publish_date)
     @publisher = publisher
     @cover_state = cover_state
   end
