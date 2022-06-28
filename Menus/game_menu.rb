8a807eb29872048010179d8dd25c20b4095b9999
Kemigabo Catherine <kemigabocatherine205@gmail.com>
2022-06-29 01:05:31 +0300

    Add games menu

--- Menus/game_menu.rb
+++ Menus/game_menu.rb
@@ -1,14 +1,24 @@
+require './MenuActions/show_lists'
+
 class GameMenu
   def initialize
+    @games = load_games
     @status = true
     @welcome_message = [
       'Welcome to the game menu!',
       'Please choose an option by entering a number:',
-      '1 - Option 1',
-      '2 - Option 2',
-      '3 - Option 3',
+      '1 - List all games',
+      '2 - List all genres(e.g \'Sandbox\', \'Shooters\', \'Action-adventure\')',
+      '3 - Add a game',
       '4 - Return to main menu'
     ]
+  end
+
+  def game_genres
+    game_genres = []
+    @games.each do |game|
+      game_genres << game.genre
+    end
   end
 
   def run
@@ -17,10 +27,12 @@ class GameMenu
       option = gets.chomp
 
       case option
-      when '1' then puts 1
-      when '2' then puts 2
-      when '3' then puts 3
-      when '4' then @status = false
+      when '1' then ShowLists.new.list_all_games(@games)
+      when '2' then ShowLists.list_game_genres(game_genres)
+      when '3' then CreateGame.new.create_game(@games)
+      when '4'
+        save_games
+        @status = false
       else
         puts "Sorry, you choose a wrong option\n "
       end
