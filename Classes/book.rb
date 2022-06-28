794f0b221b330bfdb03bfa689eeefd6b0a816201
sediqullahbadakhsh <sediqullah.badakhsh@gmail.com>
2022-06-28 20:34:19 +0430

    add can_be_archived? method to the book class

--- Classes/book.rb
+++ Classes/book.rb
@@ -4,9 +4,12 @@ class Book < Item
   attr_accessor :publisher, :cover_state, :id
 
   def initialize(publisher, cover_state, publish_date)
-    super(publish_date)
-    @id = SecureRandom.uuid
+    super(id, publish_date)
     @publisher = publisher
     @cover_state = cover_state
+  end
+
+  def can_be_archived?
+    (@cover_state == 'bad' || super)
   end
 end
