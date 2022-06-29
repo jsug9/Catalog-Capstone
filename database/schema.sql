d20d7aa121be12ff46814b671e8a7f6446efd7e7
Augusto <jesus_augga_99@icloud.com>
2022-06-29 17:15:43 -0500

    fix schema sql

--- database/schema.sql
+++ database/schema.sql
@@ -1,22 +1,39 @@
 CREATE DATABASE catalog_capstone;
 
+-- Genres Table
 CREATE TABLE genres (
     id integer GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     name text
 );
 
+CREATE UNIQUE INDEX item_pkey ON item(id int4_ops);
+
+-- Label Table
+CREATE TABLE label(
+  id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  title VARCHAR(100),
+  color VARCHAR(100)
+);
+
 CREATE UNIQUE INDEX genres_pkey ON genres(id int4_ops);
+
+-- Author
+CREATE TABLE author (
+  id int PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  first_name varchar(100),
+  last_name varchar(100)
+);
+
+CREATE UNIQUE INDEX author_pkey ON author(id int4_ops);
 
 CREATE TABLE item (
     id integer GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     genre_id integer REFERENCES genres(id),
-    author_id integer,
-    label_id integer,
+    label_id integer REFERENCES label(id),
+    author_id integer REFERENCES author(id),
     publish_date date,
     archived boolean
 );
-
-CREATE UNIQUE INDEX item_pkey ON item(id int4_ops);
 
 CREATE TABLE music_albums (
     id integer REFERENCES item(id),
@@ -24,32 +41,14 @@ CREATE TABLE music_albums (
     on_spotify boolean
 );
 
-
---Lable Table
-CREATE TABLE label(
-  id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-  title VARCHAR(100),
-  color VARCHAR(100)
-);
-
 -- Book Table
 CREATE TABLE book(
-  id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  id integer REFERENCES item(id),
   publish_date DATE,
   publisher VARCHAR(100),
   cover_state VARCHAR(10),
-  genre_id INT,
-  author_id INT,
   label_id INT,
-  FOREIGN KEY (genre_id) REFERENCES genre(id) ON DELETE SET NULL ON UPDATE CASCADE,
-  FOREIGN KEY (author_id) REFERENCES author(id) ON DELETE SET NULL ON UPDATE CASCADE,
   FOREIGN KEY (label_id) REFERENCES label(id) ON DELETE SET NULL ON UPDATE CASCADE
-);
-
-CREATE TABLE author (
-  id int PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-  first_name varchar(100),
-  last_name varchar(100)
 );
 
 CREATE TABLE game (
